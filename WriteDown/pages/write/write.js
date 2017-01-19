0c23382cf1960c4ee78c7be4c73ee976264a1d9d
AlanWang <alanwangmodify@126.com>
2017-01-19 11:23:32 +0800

    temp commit

--- WriteDown/pages/write/write.js
+++ WriteDown/pages/write/write.js
@@ -21,6 +21,24 @@ Page({
     
   },
 
+  chooseImg:function (){
+      console.log("选择图片")
+      wx.chooseImage({
+        count: 9, // 最多可以选择的图片张数，默认9
+        sizeType: ['original', 'compressed'], // original 原图，compressed 压缩图，默认二者都有
+        sourceType: ['album', 'camera'], // album 从相册选图，camera 使用相机，默认二者都有
+        success: function(res){
+          var tempFilePaths = res.tempFilePaths
+          console.log(tempFilePaths)
+        },
+        fail: function() {        
+        },
+        complete: function() {
+          // complete
+        }
+      });
+      
+  },
  
   SaveBtnEventHandle:function (){
     try {
